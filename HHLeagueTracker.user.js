7e4371652b2639ec460b33227945d84f95e4fc35
xnh0x <xnh0x@pm.me>
2025-10-29 17:46:27 +0100

    version 1.16.0

--- HHLeagueTracker.user.js
+++ HHLeagueTracker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         HH League Tracker
-// @version      1.15.0
+// @version      1.16.0
 // @description  Highlight stat changes, track lost points
 // @author       xnh0x
 // @match        https://*.hentaiheroes.com/leagues.html*
